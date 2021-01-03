d4bd772a4d826fac65f61d62a8e4917efa292c68
muntari29 <muntrock@gmail.com>
2021-01-03 18:48:51 +0900

    [15기 문승희] Add #4 메인페이지 부가서비스 번들 뷰 및 유닛테스트 기능 구현

--- flight/tests.py
+++ flight/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import unittest, json
+from django.test   import TestCase, Client
+from flight.models import Service
 
-# Create your tests here.
+class ServiceBundleTest(TestCase):
+    def setUp(self):
+        Service.objects.create(
+                name      = '면세품',
+                image_url = 'https://www.flaticon.com/svg/static/icons/svg/869/869104.svg'
+                )
+
+    def tearDown(self):
+        Service.objects.all().delete()
+
+    def test_flight_servicebundle_view_success(self):
+        client   = Client()
+        response = client.get('/flight/service', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+                'SERVICE' : [
+                    {
+                        'name'  : '면세품',
+                        'image' : 'https://www.flaticon.com/svg/static/icons/svg/869/869104.svg'
+                        }
+                    ]
+            }
+        )
+
+
