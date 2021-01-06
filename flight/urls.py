92ef7dbcf6ef0afb27caf5fc8528f804f1d78fd1
muntari29 <muntrock@gmail.com>
2021-01-07 01:48:12 +0900

    [15기 문승희] Add 메인 국가별 노선정보 API 및 유닛테스트 기능 구현

--- flight/urls.py
+++ flight/urls.py
@@ -1,7 +1,10 @@
 from django.urls  import path
-from flight.views import ServiceBundleView, PassengerInformationView
+from flight.views import ServiceBundleView, FlightImageView, MainFlightView, MainCountryView, PassengerInformationView
 
 urlpatterns = [
         path('/service',ServiceBundleView.as_view()), 
+        path('/flight_image', FlightImageView.as_view()),
         path('/passenger', PassengerInformationView.as_view()),
+        path('/<int:country_id>', MainFlightView.as_view()),
+        path('/country' , MainCountryView.as_view())
 ]
